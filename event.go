02e1590d65a2b3f6160f25783e8b4cbcaaa16baf
Halfwit <michaelmisch1985@gmail.com>
2017-03-31 14:56:13 -0600

    Trying something slightly different to get a file we can tail

--- event.go
+++ event.go
@@ -7,45 +7,34 @@ import (
 
 type event struct {
 	client string
-	mtime  time.Time
 	wait   chan []byte
-	done   chan struct{}
-	off    int64
+	u      *Srv
 }
 
 // Read in a loop, writing to b as we get more data
-func (e *event) Read(b []byte) (n int, err error) {
-	for {
-		select {
-		case msg := <-e.wait:
-			copy(b, msg)
-		}
-	}
-	return 0, nil
+func (e *event) Read(p []byte) (int, error) {
+	c := <-e.wait
+	n := copy(p, c)
+	return n, nil
 }
 
 func (e *event) Close() error {
-	close(e.done)
+	e.u.Lock()
+	delete(e.u.event, e.client)
+	defer e.u.Unlock()
 	return nil
 }
 
-func (e *event) Size() int64        { return e.off }
+func (e *event) Size() int64        { return 1 }
 func (e *event) Name() string       { return "event" }
-func (e *event) ModTime() time.Time { return e.mtime }
+func (e *event) ModTime() time.Time { return time.Now() }
 func (e *event) Mode() os.FileMode  { return 0400 }
 func (e *event) IsDir() bool        { return false }
 func (e *event) Sys() interface{}   { return nil }
 
 func (u *Srv) readEvent(user string) *event {
+	u.Lock()
 	u.event[user] = make(chan []byte)
-	done := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case <-done:
-				delete(u.event, user)
-			}
-		}
-	}()
-	return &event{client: user, mtime: time.Now(), done: done, wait: u.event[user], off: 0}
+	defer u.Unlock()
+	return &event{u: u, client: user, wait: u.event[user]}
 }
