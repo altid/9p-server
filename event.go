a0279721e9082baa8e74f692f75df17318c8c352
Halfwit <michaelmisch1985@gmail.com>
2017-04-19 01:27:28 -0600

    Update variable names

--- event.go
+++ event.go
@@ -12,7 +12,7 @@ type Event struct {
 func (e *Event) ReadAt(p []byte, off int64) (int, error) {
 	// Block on initial read until event occurs
 	if off == 0 {
-		e.data = <-e.u.event[e.s]
+		e.data = <-e.u.event[e.client]
 	}
 	n := copy(p, e.data[off:])
 	return n, nil
@@ -22,7 +22,7 @@ func (e *Event) Close() {
 	// Remove from event map
 	e.u.Lock()
 	defer e.u.Unlock()
-	delete(e.u.event, e.s)
+	delete(e.u.event, e.client)
 }
 
 func newEvent(u *Srv, client string) *Event {
