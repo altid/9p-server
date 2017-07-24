6dc1918edeac8db08213a2c1afcd1df0bcda3aa8
Halfwit <michaelmisch1985@gmail.com>
2017-07-24 14:46:18 -0600

    Add a bit of skeleton code

--- server.go
+++ server.go
@@ -4,6 +4,18 @@ import (
 	"github.com/mortdeus/go9p/srv"
 )
 
-func NewSrv() (*srv.Fsrv, error) {
-	
+// Init work for fakefiles
+// Init work for clients and server
+func init() {
+
+}
+
+// Serve - Listen and serve client connections
+func Serve() {
+	// After data is initialized for fakefiles, start listening on port for connections
+	// Check if connection exists in map, add to map so we can track otherwise.
+	err = srv.StartNetListener("tcp", *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
