4096778ab4c8b6c8f2d4e1c60899c4e8e7e5405f
Halfwit <michaelmisch1985@gmail.com>
2018-02-23 18:48:34 -0700

    Found an approach that should work properly here
    Signed-off-by: Halfwit <michaelmisch1985@gmail.com>

--- fakefile.go
+++ fakefile.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-	"log"
-	"github.com/mortdeus/go9p/srv"
+//	"log"
+//	"github.com/mortdeus/go9p/srv"
 )
+// open foo site | LIST FILES | ADD FILES (use other style of 9p server)
+// Then, add inotifywatch | add/delete files
+// buffer foo site | list files | add files
+// close - delete inotifywatch
+// Amount of watches increases with clients - stress this some
+// We need to figure out if this is tunable per-client.
+// We could have a seperate root per client connected
 
 // TODO: Research ordering of connOpened and attach requests from go9p
 /* TODO: When we encounter one of these files, we need to synthesize the response; else we pass the regular requested file along, if it exists 
@@ -22,62 +29,3 @@ Normalfiles (
 	// add list of Normalfiles we want to check for
 	// - if file already exists in our dir object, don't add. (map by string)
 */
-
-type Fakefile struct {
-	// TODO: Define our struct for fakefile
-	name string
-}
-
-func (f *Fakefile) ConnOpened(conn *srv.Conn) {
-	if *debug > 0 {
-		log.Println("Connected")
-	}
-	// TODO: Handle client setup
-	// Client will be handed the default (generally last used) buffer
-	// Requires DefaultFile as well
-}
-
-func (f *Fakefile) ConnClosed(conn *srv.Conn) {
-	if *debug > 0 {
-		log.Println("Disconnected")
-	}
-	// TODO: Handle client cleanup
-}
-
-func (f *Fakefile) FidDestroy(sfid *srv.Fid) {
-	// TODO: Clean up tabs, clients
-}
-
-// Attach - Client attaches, set up 
-func (f *Fakefile) Attach(req *srv.Req) {
-	// TODO: If we have a qualified path on the attach request, hand client access only to that requested directory
-	if *debug > 0 {
-		log.Println("Attached")
-	}
-}
-
-// Read - switch for ctl, input, tabs, etc to fabricate content
-func (f *Fakefile) Read(req *srv.Req) {
-	// TODO: Implement main read function, which will synthesize content or pass along fd of existing files
-}
-
-// Write - Make sure we grab our content we want
-func (f *Fakefile) Write(req *srv.Req) {
-	// TODO: Implement main write function, which will handle control and input to our underlying filesystems, or pass along to fd of existing files
-}
-
-// Wstat - flush stat, A no-op for now. 
-func (f *Fakefile) Wstat(req *srv.Req) {
-	// TODO: Implement for all normal files
-}
-
-// Open - Just OpenFile really, it'll match our buffer, fallback, or fail.
-func (f *Fakefile) Open(req *srv.Req) {
-	// TODO: Wrap OpenFile
-}
-
-func (f *Fakefile) Stat(req *srv.Req) {
-	// TODO: Synthesize stat or os.Stat on a regular fd
-}
-
-// TODO: See if there's any case where `create` or `remove` on the client side is beneficial
